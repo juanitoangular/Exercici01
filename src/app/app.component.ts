dab4856a9f36f6472b9e2a72fdec2b259e5c8c04
Juanito <jfagpdm@gmail.com>
2019-06-26 18:33:10 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ex01-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Exercici01';
+}
